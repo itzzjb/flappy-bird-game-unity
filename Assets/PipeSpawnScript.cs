01cf5724ed9424e90e93e8b81d4ff104c4bd25da
Januda Bethmin <desilvabethmin@gmail.com>
2024-06-16 21:16:07 +0530

    Creating a pipe prefab and a new object pipe spawner

---
+++ Assets/PipeSpawnScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+// We dragged and dropped the previously created pipe object to the Assets
+// This created a prefabricated game object or prefab
+// This is like a blueprint for a game object
+// We can create new version of this entire game object with all of it's children , components and properties.
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
