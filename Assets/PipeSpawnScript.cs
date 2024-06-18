4eefc9add75d8535a34cdf090e6f9512425328ce
Januda Bethmin <desilvabethmin@gmail.com>
2024-06-18 13:44:12 +0530

    Spawning pipes in intervals

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -11,6 +11,15 @@ public class PipeSpawnScript : MonoBehaviour
     // First we need to create a reference to the prefab
     public GameObject pipe;
 
+    // We need to more variables to following
+
+    // How many seconds between spawns
+    public float spawnRate = 2;
+
+    // Number that counts up (time)
+    // We can make this private becuase we won't be changing this in the editor or anywhere else
+    private float timer = 0;
+
     void Start()
     {
         
@@ -18,6 +27,20 @@ public class PipeSpawnScript : MonoBehaviour
 
     void Update()
     {
-        // We are 
+        // We are going to use a if condition to 
+
+        if (timer < spawnRate)
+        {
+            // Creating a number that counts up no matter the frame rate.
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            // We can use the following method to spawn game objects
+            // By using transform.postion and transform. rotation we are using the same position and the rotation as the spawner
+            Instantiate(pipe, transform.position, transform.rotation);
+            // When the timer is equal to spawn rate it will be reset to 0
+            timer = 0;
+        }
     }
 }
