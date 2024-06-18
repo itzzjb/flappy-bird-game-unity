13198d8d47973bd8cf3ffafd9293072144d6f777
Januda Bethmin <desilvabethmin@gmail.com>
2024-06-18 18:48:40 +0530

    adding a deadzone to the moving pipes

--- Assets/PipeMovesScript.cs
+++ Assets/PipeMovesScript.cs
@@ -9,6 +9,11 @@ public class PipeMovesScript : MonoBehaviour
     // We can give a default value for the variable and change the value from the UI if needed
     public float moveSpeed = 5;
 
+    // When new and new pipes gets created from the pipeSpawner they will be there until the aplication is stopped.
+    // So even after we can't see the pipe from the displa it will continuesly using resources of the device
+    // We can define a deadzone by giving a x value (When the pipe reaches that x value it will be deleted)
+    public float deadZone = -45;
+
     void Start()
     {
         
@@ -21,5 +26,13 @@ public class PipeMovesScript : MonoBehaviour
         // We can multiply the value to create more values (moveSpeed = Multiplier)
         // By multiplying by Time.deltaTime, it ensures that 
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        // When the pipe reaches that x value it will be deleted
+        if (transform.position.x < deadZone)
+        {
+            // Destoying the pipe that was created using this template
+            Destroy(gameObject);
+        }
     }
 }
+
